fafbeba6a67d5961755d0ec210b409b817b1c328
Manuel González <mgmerino@gmail.com>
2023-08-16 00:12:54 +0200

    Fix formatting

--- test/binary_tree_test.rb
+++ test/binary_tree_test.rb
@@ -1,5 +1,4 @@
 require 'minitest/autorun'
-require 'debug'
 require_relative '../models/grid'
 require_relative '../models/binary_tree'
 
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -7,7 +7,7 @@ class GridTest < Minitest::Test
     assert_equal grid.rows, 2
     assert_equal grid.columns, 2
     assert_equal grid.size, 4 
-
+    
     assert grid.random_cell.is_a?(Cell)
     assert grid[0, 0].is_a?(Cell)
     assert grid[grid.rows+1, grid.columns+1].nil?
