34176b5a5bd04f3be6f567a5b1fdd313d4d3ecf3
Manuel González <mgmerino@gmail.com>
2023-08-15 23:33:33 +0200

    Fix grid tests

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -1,10 +1,15 @@
 require 'minitest/autorun'
-require_relative '../grid'
+require_relative '../models/grid'
 
 class GridTest < Minitest::Test
   def test_grid_initialization
     grid = Grid.new(2, 2)
-    assert grid.rows == 2
-    assert grid.columns == 2
+    assert_equal grid.rows, 2
+    assert_equal grid.columns, 2
+    assert_equal grid.size, 4 
+
+    assert grid.random_cell.is_a?(Cell)
+    assert grid[0, 0].is_a?(Cell)
+    assert grid[grid.rows+1, grid.columns+1].nil?
   end
 end
