2c0a5cbd0fe428828d9e9bc4d9c0ae694be32439
Manuel González <mgmerino@gmail.com>
2023-08-16 00:11:34 +0200

    Add test for binary tree

--- test/binary_tree_test.rb
+++ test/binary_tree_test.rb
@@ -1,9 +1,28 @@
 require 'minitest/autorun'
+require 'debug'
 require_relative '../models/grid'
 require_relative '../models/binary_tree'
 
 class BinaryTreeTest < Minitest::Test
-  def test_binary_tree_creation
-    # TODO: test that the binary tree is created correctly
+  def setup
+    srand(12345) # Set a fixed seed
+  end
+  
+  def test_generates_maze_for_small_grid
+    grid = Grid.new(2, 2)
+    BinaryTree.on(grid)
+    
+    # Check specific cell linkages based on binary tree algorithm
+    assert(grid[1, 1].linked?(grid[0, 1]))
+    assert(grid[1, 1].linked?(grid[1, 0]))
+  end
+  
+  def test_generates_maze_without_invalid_linkages
+    grid = Grid.new(4, 4)
+    BinaryTree.on(grid)
+    
+    # Check that cells are only linked in valid directions
+    assert(grid[1, 1].linked?(grid[1, 2]) || grid[1, 1].linked?(grid[2, 1]))
+    assert(grid[3, 3].linked?(grid[3, 2]) || grid[3, 3].linked?(grid[2, 3]))
   end
 end
